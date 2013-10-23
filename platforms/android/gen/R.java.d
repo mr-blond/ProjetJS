2018249366dc4f2549d82eb8a18c9d3c321c6382
mr-blond <psid.v1@gmail.com>
2013-10-23 10:32:03 +0200

    Projet de base
    - partage d'info entre serveur et application mobile (penser à changer
    adresse IP du serv

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\gen\fr\gobelins\projetbiennale\R.java \
+ : C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\drawable\icon.png \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\values\strings.xml \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\res\xml\config.xml \
+C:\Users\julien\Desktop\ProjetBiennale\platforms\android\bin\AndroidManifest.xml \
