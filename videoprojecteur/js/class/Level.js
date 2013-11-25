631c5487f1ff627687d9cd43f1b17faadb7755fb
StefanPATRICE <stefan.patrice@gmail.com>
2013-11-25 20:30:14 +0100

    Rollback

--- videoprojecteur/js/class/Level.js
+++ videoprojecteur/js/class/Level.js
@@ -6,20 +6,20 @@ Level.init = function()
 {
     Level.json = {
         "palier": [
-			//Cadre de l'écran
-			/*
-            { x: -50, y: Const.height / 2, width: Level.largeurCadre, height: Const.height + Level.largeurCadre * 2 },
-            { x: Const.width + Level.largeurCadre / 2, y: Const.height / 2, width: Level.largeurCadre, height: Const.height },
-            { x: Const.width / 2, y: -50, width: Const.width + Level.largeurCadre * 2, height: Level.largeurCadre },
-            { x: Const.width / 2, y: Const.height + Level.largeurCadre / 2, width: Const.width + Level.largeurCadre * 2, height: Level.largeurCadre },
-			*/
+            //Cadre de l'écran
+            /*
+             { x: -50, y: Const.height / 2, width: Level.largeurCadre, height: Const.height + Level.largeurCadre * 2 },
+             { x: Const.width + Level.largeurCadre / 2, y: Const.height / 2, width: Level.largeurCadre, height: Const.height },
+             { x: Const.width / 2, y: -50, width: Const.width + Level.largeurCadre * 2, height: Level.largeurCadre },
+             { x: Const.width / 2, y: Const.height + Level.largeurCadre / 2, width: Const.width + Level.largeurCadre * 2, height: Level.largeurCadre },
+             */
 
-			//Angle du bas
+            //Angle du bas
             { x: Const.width * 1/4, y: Const.height, width: Const.width / 2 + 7, height: Level.largeurCadre / 2, angle: -0.1 },
-            { x: Const.width * 1/4 - 50, y: Const.height - 85, width: Const.width / 2 + 83, height: 10, angle: 0.1 },
+            { x: Const.width * 1/4 - 50, y: Const.height - 80, width: Const.width / 2 + 83, height: 10, angle: 0.1 },
             { x: Const.width * 3/4, y: Const.height, width: Const.width / 2 + 7, height: Level.largeurCadre / 2, angle: 0.1 },
 
-			//Palier déplacable
+            //Palier déplacable
             { x: 400, y: 100, width: 100, height: 10, angle: -0.1 },
             { x: 300, y: 170, width: 100, height: 10, angle: 0.1 },
             { x: 450, y: 400, width: 100, height: 10, angle: 0.1 }
@@ -30,7 +30,7 @@ Level.init = function()
         var palier = new Palier();
         palier.init(entry);
     });
-    GenerateurGouttelette.create({cooldown: 10, x: Const.width / 2});
+    GenerateurGouttelette.create({cooldown: 15, x: Const.width / 2});
 };
 Level.rotate = function(angle)
 {
